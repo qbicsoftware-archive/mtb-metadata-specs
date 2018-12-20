01fe858127ea29653efa828b525c9cfe482e4f88
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2018-12-20 21:28:28 +0100

    Refactor tracking reference

--- test/mtb/finding_dna_schema.groovy
+++ test/mtb/finding_dna_schema.groovy
@@ -9,9 +9,11 @@ import org.everit.json.schema.loader.SchemaLoader
 import org.json.JSONObject
 import org.json.JSONTokener
 
+def projectRootDir = args ? args[0] : "."
 
-InputStream inputStream = new FileInputStream("../../schemes/mtb/finding.dna.schema.json")
+
+InputStream inputStream = new FileInputStream("${projectRootDir}/schemes/mtb/finding.dna.schema.json")
 JSONObject rawSchema = new JSONObject(new JSONTokener(inputStream));
-InputStream exampleJsonStream = new FileInputStream("finding.dna.example.json")
+InputStream exampleJsonStream = new FileInputStream("${projectRootDir}/test/mtb/finding.dna.example.json")
 Schema schema = SchemaLoader.load(rawSchema);
 schema.validate(new JSONObject(new JSONTokener(exampleJsonStream))) // throws a ValidationException if this object is invalid
