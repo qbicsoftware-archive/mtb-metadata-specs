1d1c901440e6d5b3772a31da365203cc9f14e12c
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2019-01-14 09:54:08 +0100

    Provides example schema

--- schemes/example/validate.groovy
+++ schemes/example/validate.groovy
@@ -10,8 +10,8 @@ import org.json.JSONObject
 import org.json.JSONTokener
 
 
-InputStream inputStream = new FileInputStream("example.schema.json")
+InputStream inputStream = new FileInputStream("/Users/sven1103/git/mtb-data-specs/schemes/example/example.schema.json")
 JSONObject rawSchema = new JSONObject(new JSONTokener(inputStream));
-InputStream exampleJsonStream = new FileInputStream("example.object.json")
+InputStream exampleJsonStream = new FileInputStream("/Users/sven1103/git/mtb-data-specs/schemes/example/example.object.json")
 Schema schema = SchemaLoader.load(rawSchema);
 schema.validate(new JSONObject(new JSONTokener(exampleJsonStream))) // throws a ValidationException if this object is invalid
