15391ce447ec753b3c7a9054b59dd1d88ef65208
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2019-01-15 13:30:55 +0100

    Extends DNA finding example

--- test/mtb/finding_dna_schema.groovy
+++ test/mtb/finding_dna_schema.groovy
@@ -4,6 +4,7 @@
     @Grab(group='com.github.everit-org.json-schema', module='org.everit.json.schema', version='1.10.0')
 )
 
+import org.everit.json.schema.ValidationException
 import org.everit.json.schema.Schema
 import org.everit.json.schema.loader.SchemaLoader
 import org.json.JSONObject
@@ -13,7 +14,16 @@ def projectRootDir = args ? args[0] : "."
 
 
 InputStream inputStream = new FileInputStream("${projectRootDir}/schemes/mtb/finding.dna.schema.json")
+//InputStream inputStream = new FileInputStream("${projectRootDir}/schemes/mtb/definitions/clinical.common.json")
 JSONObject rawSchema = new JSONObject(new JSONTokener(inputStream));
 File example = new File("${projectRootDir}/test/mtb/finding.dna.example.json")
 Schema schema = SchemaLoader.load(rawSchema)
-schema.validate(new JSONObject(new JSONTokener(example.newDataInputStream()))) // throws a ValidationException if this object is invalid
+try {
+  schema.validate(new JSONObject(new JSONTokener(example.newDataInputStream()))) // throws a ValidationException if this object is invalid
+} catch (ValidationException e) {
+    System.out.println(e.getMessage())
+    e.getCausingExceptions().each {
+        print "${it.getAllMessages()}\n"
+    }
+}
+
